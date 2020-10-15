f751d29b221175859d463f8b077bd0269b17e5a8
nhquang <quangpersie1@gmail.com>
2020-10-14 22:47:15 -0400

    better solution for topKFrequent()

--- TopKFrequent.cs
+++ TopKFrequent.cs
@@ -1,7 +1,7 @@
 		//https://leetcode.com/problems/top-k-frequent-elements/
 		
 		
-		public static int[] TopKFrequent(int[] nums, int k)
+		/*public static int[] TopKFrequent(int[] nums, int k)
         {
             var pairs = new Dictionary<int, int>();
             for (int i = 0; i < nums.Length; i++)
@@ -17,4 +17,135 @@
             }
             return rslt.ToArray();
 
+        }*/
+		
+		public class Solution {
+		public int[] TopKFrequent(int[] nums, int k)
+        {
+            var pairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (pairs.ContainsKey(nums[i])) pairs[nums[i]]++;
+                else pairs.Add(nums[i], 1);
+            }
+            var sortedByValue = new MySortedDictionary();
+            foreach(var pair in pairs){
+                sortedByValue.Add(pair);
+            }
+            var rslt = new List<int>();
+            for(int i = 0; i < k; i++)
+            {
+                rslt.Add(sortedByValue.popTail().Key);
+            }
+            return rslt.ToArray();
+            
         }
+        
+	}
+	class MySortedDictionary
+    {
+        public class Node
+        {
+            
+            public KeyValuePair<int, int> KeyValuePair_;
+            public Node next_;
+            public Node prev_;
+            public Node(KeyValuePair<int,int> keyValuePair, Node next, Node prev)
+            {
+                KeyValuePair_ = keyValuePair;
+                next_ = next;
+                prev_ = prev;
+            }
+        }
+        Node root_;
+        Node tail_;
+        public int length { get; set; }
+        
+
+        public MySortedDictionary()
+        {
+            root_ = null;
+            length = 0;
+        }
+        public void calculateLength()
+        {
+            this.length = 0;
+            Node current = root_;
+            while (current != null)
+            {
+                length++;
+                current = current.next_;
+            }
+        }
+        public void Add(KeyValuePair<int,int> newKeyValuePair)
+        {
+            
+            var newNode = new Node(newKeyValuePair, null, null);
+            if (root_ == null) { root_ = newNode; tail_ = newNode;  length++; }
+            else
+            {
+                Node current = root_;
+
+                while (current != null)
+                {
+                    if(current.next_ == null && newKeyValuePair.Value > current.KeyValuePair_.Value)
+                    {
+                        current.next_ = newNode;
+                        current.next_.prev_ = current;
+                        tail_ = current.next_;
+                        length++;
+                        return;
+                    }
+                    if(newKeyValuePair.Value <= current.KeyValuePair_.Value)
+                    {
+                        if(current.prev_ == null)
+                        {
+                            current.prev_ = newNode;
+                            current.prev_.next_ = current;
+                            root_ = current.prev_;
+                        }
+                        else
+                        {
+                            current.prev_.next_ = newNode;
+                            current.prev_.next_.prev_ = current.prev_;
+                            current.prev_.next_.next_ = current;
+                            current.prev_ = current.prev_.next_;
+                        }
+                        
+                        length++;
+                        return;
+                    }
+                    current = current.next_;
+
+                }
+            }
+        }
+        public KeyValuePair<int,int> popTail()
+        {
+            KeyValuePair<int, int> rslt = new KeyValuePair<int,int>();
+            if (tail_ == null) return rslt;
+            if (tail_.prev_ == root_)
+            {
+                root_.next_ = null;
+                rslt = tail_.KeyValuePair_;
+                tail_ = root_;
+            }
+            else if (tail_.prev_ == null)
+            {
+                rslt = tail_.KeyValuePair_;
+                root_ = tail_ = null;
+            }
+            else
+            {
+                tail_.prev_.next_ = null;
+                rslt = tail_.KeyValuePair_;
+                tail_ = tail_.prev_;
+            }
+            length--;
+            
+            return rslt;
+        }
+        
+
+	}
+        
