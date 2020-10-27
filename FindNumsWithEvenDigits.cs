c07e4e83621005ba0981502839c18b633a558edb
nhquang <quangpersie1@gmail.com>
2020-10-27 00:48:50 -0400

    Nums with even number of digits

---
+++ FindNumsWithEvenDigits.cs
@@ -0,0 +1,19 @@
+	//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+
+
+	public int FindNumbers(int[] nums) {
+        int count = 0;
+        
+        for(int i = 0; i < nums.Length; i++){
+            //if(nums[i].ToString().Length % 2 == 0) count++;
+            int count2 = 1, temp = 10;
+            while(nums[i] != 0){
+                nums[i] /= temp;
+                if(nums[i] == 0)
+                    if(count2 % 2 == 0) count++;
+                count2++;
+            }
+        }
+        return count;
+    }
