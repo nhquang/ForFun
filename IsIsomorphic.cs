fcddd8d2ca47264f1ee12c0dc75698521fe5358b
nhquang <quangpersie1@gmail.com>
2020-11-07 14:36:15 -0500

    better solution for IsIsomorphic

--- IsIsomorphic.cs
+++ IsIsomorphic.cs
@@ -1,30 +1,17 @@
 	//https://leetcode.com/problems/isomorphic-strings/
 	
 	
-	public bool IsIsomorphic(string s, string t)
+		public bool IsIsomorphic(string s, string t)
         {
-            var dict1 = new Dictionary<char, int>();
-            var dict2 = new Dictionary<char, int>();
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (dict1.ContainsKey(s[i])) dict1[s[i]]++;
-                else dict1.Add(s[i], 1);
-                if (dict2.ContainsKey(t[i])) dict2[t[i]]++;
-                else dict2.Add(t[i], 1);
+            var matches = new Dictionary<char, char>();
+			for(int i = 0; i < s.Length; i++){
+            if(!matches.ContainsKey(s[i])){
+                if(!matches.ContainsValue(t[i]))  matches.Add(s[i],t[i]);
+                else return false;
             }
-            if (dict1.Count != dict2.Count) return false;
-            for (int i = 0; i < dict1.Count; i++)
-            {
-                if (dict1.ElementAt(i).Value != dict2.ElementAt(i).Value) return false;
-                else
-                {
-                    for(int j = 0; j < s.Length; j++)
-                    {
-                        if (s[j] == dict1.ElementAt(i).Key)
-                            if (t[j] != dict2.ElementAt(i).Key)
-								return false;
-                    }
-                }
-            }
-            return true;
+            else
+                if(matches[s[i]] != t[i]) return false;
+			}
+        
+			return true;
         }
