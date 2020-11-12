a76d3d72f20dd7d4f8e09c5fb9cf17adf793f96c
nhquang <quangpersie1@gmail.com>
2020-11-11 20:19:28 -0500

    indent

--- JewelsAndStones.cs
+++ JewelsAndStones.cs
@@ -1,7 +1,7 @@
 		//https://leetcode.com/problems/jewels-and-stones/
 
 
-public int NumJewelsInStones(string J, string S) {
+    public int NumJewelsInStones(string J, string S) {
         int count = 0;
         for(int i = 0; i < S.Length; i++){
             for(int j = 0; j < J.Length; j++){
