107c07ee8590fe59103b107b68f0f0f830850023
nhquang <quangpersie1@gmail.com>
2020-11-15 19:31:58 -0500

    indent

--- IsIsomorphic.cs
+++ IsIsomorphic.cs
@@ -1,17 +1,18 @@
 	//https://leetcode.com/problems/isomorphic-strings/
 	
 	
-		public bool IsIsomorphic(string s, string t)
-        {
-            var matches = new Dictionary<char, char>();
-			for(int i = 0; i < s.Length; i++){
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        var matches = new Dictionary<char, char>();
+        for(int i = 0; i < s.Length; i++){
             if(!matches.ContainsKey(s[i])){
                 if(!matches.ContainsValue(t[i]))  matches.Add(s[i],t[i]);
                 else return false;
             }
             else
                 if(matches[s[i]] != t[i]) return false;
-			}
-        
-			return true;
         }
+        
+        return true;
+    }
+}
