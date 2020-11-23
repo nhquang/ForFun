ca1007e34b762757091130e091c97706a6fa39a2
nhquang <quangpersie1@gmail.com>
2020-11-22 22:32:06 -0500

    Reverse Linked List

---
+++ ReverseLinkedList.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    
+    public ListNode ReverseList(ListNode head) {
+        var current = head;
+        ListNode newHead = null;
+        
+        while(current != null){
+            if(current == head)
+                newHead = new ListNode(current.val, null);
+            else
+                newHead = new ListNode(current.val, newHead);
+			
+            current = current.next;
+        }
+        return newHead;
+    }
+	public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+}
