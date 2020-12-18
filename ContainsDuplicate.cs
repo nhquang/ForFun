ddafe7d3617672a0c8d4cbf97d09f9a69ffbef06
nhquang <quangpersie1@gmail.com>
2020-12-18 14:11:58 -0500

    0(n) solution for contains dups

--- ContainsDuplicate.cs
+++ ContainsDuplicate.cs
@@ -1,13 +1,27 @@
 	//https://leetcode.com/problems/contains-duplicate/
 	
+	public class Solution {
+		
+		//O(n)
+		public bool ContainsDuplicate(int[] nums) {
+			var set = new HashSet<int>();
+			for(int i = 0; i < nums.Length; i++){
+				if(set.Contains(nums[i])) return true;
+				else set.Add(nums[i]);
+			}
+			return false;
+		}
+		
+		
+		public bool ContainsDuplicate(int[] nums) {
+			var distincts = new Dictionary<int,int>();
+			foreach(var i in nums){
+				if(distincts.ContainsKey(i)) distincts[i]++;
+				else distincts.Add(i, 1);
+			}
+			foreach(var item in distincts)
+				if(item.Value > 1) return true;
+			return false;
+		}
+	}
 	
-	public bool ContainsDuplicate(int[] nums) {
-        var distincts = new Dictionary<int,int>();
-        foreach(var i in nums){
-            if(distincts.ContainsKey(i)) distincts[i]++;
-            else distincts.Add(i, 1);
-        }
-        foreach(var item in distincts)
-            if(item.Value > 1) return true;
-        return false;
-    }
