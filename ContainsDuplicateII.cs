8b122f37c2e8297a178945c6831bf8f3f2b81942
nhquang <quangpersie1@gmail.com>
2020-12-18 14:05:23 -0500

    optimal solution for nearby duplicates

--- ContainsDuplicateII.cs
+++ ContainsDuplicateII.cs
@@ -4,6 +4,8 @@
 
 
 public class Solution {
+	
+	//sliding window
     public bool ContainsNearbyDuplicate(int[] nums, int k) {
         for(int i = 0; i < nums.Length; i++){
             for(int j = i+1; j < nums.Length; j++){
@@ -11,6 +13,19 @@ public class Solution {
                 if(nums[i] == nums[j])
                     return true;
             }
+        }
+        return false;
+    }
+	
+	//O(n)
+	public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        var dict = new Dictionary<int,int>();
+        for(int i = 0; i < nums.Length; i++){
+            if(dict.ContainsKey(nums[i])){
+                if((i - dict[nums[i]]) <= k) return true;
+                else dict[nums[i]] = i;
+            }
+            else dict.Add(nums[i], i);
         }
         return false;
     }
