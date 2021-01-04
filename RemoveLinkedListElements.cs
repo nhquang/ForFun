b8ebaf115e7644a63e0d7f17c194921b11a57c8e
nhquang <quangpersie1@gmail.com>
2021-01-04 13:39:48 -0500

    simplified remove linked list elements

--- RemoveLinkedListElements.cs
+++ RemoveLinkedListElements.cs
@@ -15,30 +15,18 @@
 	
 	
 	public ListNode RemoveElements(ListNode head, int val) {
-            var current = head;
-            ListNode prev = null;
-            while (current != null)
-            {
-                bool skip = false;
-                if(current.val == val)
-                {
-                    if (current.next == null)
-                    {
-                        if (current == head) current = head = null;
-                        else prev.next = current = null;
-                    }
-                    else
-                    {
-                        if (current == head) head = current.next;
-                        else{
-                            prev.next = current.next;
-                            skip = true;
-                        }
-                    }
-                }
-                if(!skip) prev = current;
-                if(current != null) current = current.next;
-                
+        var curr = head;
+        ListNode prev = null;
+        while (curr != null)
+        {
+            if(curr.val == val){
+                if(prev != null) prev.next = curr.next;
+                else head = curr.next;
+                curr = curr.next;
+                continue;
             }
-            return head;
+            prev = curr;
+            curr = curr.next;
+        }
+        return head;
     }	
