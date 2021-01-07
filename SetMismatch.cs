3c16928635423fd193f445584e04016d18fd0e22
nhquang <quangpersie1@gmail.com>
2021-01-07 14:01:18 -0500

    another solution for set mismatch

--- SetMismatch.cs
+++ SetMismatch.cs
@@ -3,6 +3,22 @@
 
 
 public class Solution {
+	
+	//O(n)
+	public int[] FindErrorNums(int[] nums) {
+        int[] rslt = new int[2];
+        var set = new HashSet<int>();
+        for(int i = 0; i < nums.Length; i++){
+            if(set.Contains(nums[i])) rslt[0] = nums[i];
+            else set.Add(nums[i]);
+        }
+        for(int i = 1; i <= nums.Length; i++){
+            if(!set.Contains(i)) rslt[1] = i;
+        }
+        return rslt;
+    }
+	
+	//0(n)without using extra space
     public int[] FindErrorNums(int[] nums) {
         int[] rslt = new int[2];
         for(int i = 0; i < nums.Length; i++){
