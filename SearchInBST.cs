0e1a68c10a9b76fbcbb37028360bd45651e52e05
nhquang <quangpersie1@gmail.com>
2021-01-31 20:53:17 -0500

    search in BST

---
+++ SearchInBST.cs
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if (root.val == val) return root;
+        else if (root.val > val && root.left != null) return SearchBST(root.left, val);
+        else if (root.val < val && root.right != null) return SearchBST(root.right, val);
+        else return null;
+    }
+}
