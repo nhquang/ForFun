d691453c0d4a597ad9b9caea280580f37baea94b
nhquang <quangpersie1@gmail.com>
2021-02-17 13:38:16 -0500

    simplified

--- LongestCommonPrefix.cs
+++ LongestCommonPrefix.cs
@@ -3,35 +3,22 @@
 
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        int j = 0;
-        char temp = '\0';
-        bool emp = false;
-        string rslt = "";
-        if (strs.Length == 1) return strs[0];
+        int j = 1;
+        if(strs.Length == 0 || strs.Length == 1) return strs.Length == 0 ? "" : strs[0];
+        foreach(var item in strs) if(item == string.Empty) return "";
+        string curr = strs[0].Substring(0,j);;
+        string rslt ="";
         for(int i = 0; i < strs.Length; i++){
-            if (i == 0)
-            {
-                if (strs[i] == "") emp = true;
-                else{
-                    if (j >= strs[i].Length) break;
-                    temp = strs[i][j];
-                }
+            string temp = "";
+            if(j <= strs[i].Length){
+                if (i == 0) curr = strs[i].Substring(0,j);
+                temp = strs[i].Substring(0,j);
             }
-            else
-            {
-                if (strs[i] == "") { if (!emp) break; }
-                else if (j < strs[i].Length && strs[i][j] != temp) break;
-                else if (j >= strs[i].Length) break;
-                if (i == strs.Length - 1)
-                {
-                    if (strs[i] == "")
-                        break;
-                    else if (j < strs[i].Length && strs[i][j] != temp) break;
-                    else if (j >= strs[i].Length) break;
-                    if (!emp) rslt += temp;
-                    emp = false;
-                    j++; i = -1;
-                }
+            else break;
+            if(temp != curr) break;
+            if(i == strs.Length - 1 && temp == curr){
+                 rslt = curr;
+                 i = -1; j++;
             }
         }
         return rslt;
