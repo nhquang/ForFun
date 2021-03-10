6a0fac3c3f33120849b18e1ab594e92547f39aae
nhquang <quangpersie1@gmail.com>
2021-03-09 22:27:25 -0500

    remove redundant code for max product of 2

--- MaxProductOf2.cs
+++ MaxProductOf2.cs
@@ -3,18 +3,14 @@
 public class Solution {
     public int MaxProduct(int[] nums) {
         int max1 = int.MinValue, max2 = int.MinValue;
-        int rslt = 1;
         for(int i = 0; i < nums.Length; i++){
             if(max1 < nums[i]){
                 max2 = max1;
                 max1 = nums[i];
-                rslt = (max1-1) * (max2-1);
             }
-            else if(max2 < nums[i]){
+            else if(max2 < nums[i])
                 max2 = nums[i];
-                rslt = (max1-1) * (max2-1);
-            }
         }
-        return rslt;
+        return (max1 - 1) * (max2 - 1);
     }
 }
