37f75d5702fe5099a25dee7751c2fdacc2811e7e
nhquang <quangpersie1@gmail.com>
2021-04-26 21:16:22 -0400

    sort arr by parity II one pass approach

--- SortArrayByParityII.cs
+++ SortArrayByParityII.cs
@@ -1,7 +1,7 @@
 //https://leetcode.com/problems/sort-array-by-parity-ii/
 //sliding window
 public class Solution {
-    public int[] SortArrayByParityII(int[] A) {
+    public int[] SortArrayByParityII_In_Place(int[] A) {
         for(int i = 0; i < A.Length; i++){
             if((i % 2) == 0 && (A[i] % 2) != 0){
                 for(int j = i + 1; j < A.Length; j++){
@@ -24,10 +24,18 @@ public class Solution {
         }
         return A;
     }
-}
-// 2 Passes
-public class Solution {
-    public int[] SortArrayByParityII(int[] A) {
+	// 1 Pass
+	public int[] SortArrayByParityIIOnePass(int[] nums) {
+        var rslt = new int[nums.Length];
+        int e = 0, o = 1;
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] % 2 == 0) { rslt[e] = nums[i]; e += 2; }
+            else { rslt[o] = nums[i]; o += 2; }
+        }
+        return rslt;
+    }
+	// 2 Passes
+	public int[] SortArrayByParityIITwoPasses(int[] A) {
         var ans = new int[A.Length];
         int j = 0;
         for(int i = 0; i < A.Length; i++){
