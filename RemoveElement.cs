7e91e651807b71dab3243755eb722f79b060d716
nhquang <quangpersie1@gmail.com>
2021-05-03 21:43:05 -0400

    better solution for remove element

--- RemoveElement.cs
+++ RemoveElement.cs
@@ -1,27 +1,21 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        if (nums.Length == 0) return 0;
-        int j = nums.Length - 1;
-        if (j == 0) return nums[0] == val ? 0 : 1;
-        int length = nums.Length;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (i >= j) break;
-            if(nums[i] == val)
-            {
-                while (j > i && nums[j] == val) j--;
-                if (j == i)
-                {
-                    if (j == 0) return 0;
+        int i = 0, j = nums.Length - 1, len = 0;
+        while(i <= j){
+            if(nums[i] == val){
+                while(i < j){
+                    if(nums[j] == val) j--;
                     else break;
                 }
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                while (j > i && nums[j] == val) j--;
+                if(i >= j) break;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                len++;
             }
+            else len++;
+            i++;
         }
-        for (int i = nums.Length - 1; i >= 0; i--) if (nums[i] == val) length--;
-        return length;
+        return len;
     }
 }
