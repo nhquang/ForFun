31cc4022c9e1e8e28fc26f3fac2aa33a1c219018
nhquang <quangpersie1@gmail.com>
2021-06-27 23:26:40 -0400

    Maximum Product Difference Between Two Pairs

---
+++ MaxProductDifference.cs
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+
+
+public class Solution {
+    public int MaxProductDifference(int[] nums) {
+        Array.Sort(nums);
+        return nums[nums.Length - 1] * nums[nums.Length - 2] - nums[0] * nums[1]; 
+    }
+}
