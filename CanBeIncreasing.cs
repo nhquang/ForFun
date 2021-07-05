72a9bde30f44d40452a4fe0d35a34d2dafe8bbf6
nhquang <quangpersie1@gmail.com>
2021-07-04 20:12:14 -0400

    CanBeIncreasing O(n) solution

--- CanBeIncreasing.cs
+++ CanBeIncreasing.cs
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
 
+
+//O(n2)
 public class Solution {
     public bool CanBeIncreasing(int[] nums) {
         int prev = 0, count = 0;
@@ -36,4 +38,22 @@ public class Solution {
         }
         return true;
     }
+}
+
+//O(n)
+public class Solution {
+    public bool CanBeIncreasing(int[] nums) {
+        int prev = 0, count = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] <= prev){
+                if(count == 1) return false;
+                if(i == 1 || nums[i - 2] < nums[i]) prev = nums[i];
+                else prev = nums[i - 1];
+                count++;
+            }
+            else prev = nums[i];
+        }
+        return true;
+    }
+    
 }
