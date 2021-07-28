b07efa56b9bb19690c3672689b6f3aebc043cc2a
nhquang <quangpersie1@gmail.com>
2021-07-28 00:20:41 -0400

    Sum of Digits of String After Convert

---
+++ SumOfDigitsOfStringAfterConvert.cs
@@ -0,0 +1,25 @@
+
+
+public class Solution {
+    public int GetLucky(string s, int k) {
+        var res = string.Empty;
+        foreach(var c in s){
+            int temp = c - '`';
+            res += temp.ToString();
+        }
+        int i = 0;
+        while(i < k){
+            var total = 0;
+            foreach(var c in res)
+                total += c - '0';
+            res = total.ToString();
+            i++;
+        }
+        int rs = 0;
+        foreach(var c in res){
+            rs *= 10;
+            rs += c - '0';
+        }
+        return rs;
+    }
+}
