11fea5b2053379f944f60df43b5c3c1640cbd393
nhquang <quangpersie1@gmail.com>
2021-08-03 13:01:01 -0400

    O(n) approach for move zeroes

--- MoveZeroes.cs
+++ MoveZeroes.cs
@@ -1,8 +1,9 @@
-	//https://leetcode.com/problems/move-zeroes/
+//https://leetcode.com/problems/move-zeroes/
 	
-	
+
+public class Solution {
+	//O(n2)
 	public void MoveZeroes(int[] nums) {
-        
         for(int i = 0; i < nums.Length - i; i++){
             for(int j = 0; j < nums.Length - 1; j++){
                 if(nums[j] == 0 && nums[j+1] != 0){
@@ -12,5 +13,15 @@
                 }
             }
         }
-        
     }
+	//O(n)
+    public void MoveZeroes(int[] nums) {
+        int a = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] != 0)
+                nums[a++] = nums[i]; 
+        }
+        while(a < nums.Length)
+            nums[a++] = 0;
+    }
+}	
