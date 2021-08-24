9752237456fbf1a110d052203a5e56eb6d03e1e7
nhquang <quangpersie1@gmail.com>
2021-08-24 16:26:57 -0400

    minimum time to type

--- MinTimeToType.cs
+++ MinTimeToType.cs
@@ -2,31 +2,10 @@
 
 public class Solution {
     public int MinTimeToType(string word) {
-        var dict = new Dictionary<char, int>();
-        dict.Add('a', 0);
-        char prev = 'a';
-        for(int i = 1; i <= 26; i++){
-            char temp = (char) (prev + 1);
-            dict.Add(temp, i);
-            prev = temp;
-        }
-        int rs = 0; char pointer = 'a';
+        int rs = 0;
+        char pointer = 'a';
         for(int i = 0; i < word.Length; i++){
-            int counterWise = 0;
-            int clockWise = 0;
-            if(pointer < word[i]) {
-                counterWise = dict[pointer] - dict['a'] + 26 - dict[word[i]] + 1;
-                clockWise = dict[word[i]] - dict[pointer] + 1;
-            }
-            else if (pointer > word[i]){
-                counterWise = dict[pointer] - dict[word[i]] + 1;
-                clockWise = 26 - dict[pointer] + dict[word[i]] - dict['a'] + 1;
-            }
-            else{
-                counterWise = 1;
-                clockWise = 1;
-            }
-            rs += Math.Min(counterWise, clockWise);
+            rs += Math.Min(Math.Abs(word[i] - pointer) + 1, 26 - Math.Abs(word[i] - pointer) + 1);
             pointer = word[i];
         }
         return rs;
